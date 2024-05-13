f6bfc82d359d538034a973da1d3a874dabc2bca9
zbyju <zbynek.jurica@gmail.com>
2024-05-13 14:56:57 +0200

    codecrafters submit [skip ci]

--- app/logic/http.go
+++ app/logic/http.go
@@ -1,7 +1,24 @@
 package logic
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"strings"
+)
 
-func HttpResponseString(version, statusCode, statusReason, headers, body string) string {
+func CreateHTTPResponseString(version, statusCode, statusReason, headers, body string) string {
 	return fmt.Sprintf("HTTP/%s %s %s\r\n%s\r\n%s", version, statusCode, statusReason, headers, body)
+}
+
+func GetHTTPRequestPath(request string) string {
+	lines := strings.Split(request, "\r\n")
+	firstLine := strings.ReplaceAll(lines[0], "  ", " ")
+	splitRequestLine := strings.Split(firstLine, " ")
+	return splitRequestLine[1]
+}
+
+func GetHTTPRequest(conn net.Conn) string {
+	buf := make([]byte, 512)
+	conn.Read(buf)
+	return string(buf[:])
 }
--- app/server.go
+++ app/server.go
@@ -9,8 +9,6 @@ import (
 )
 
 func main() {
-	fmt.Println("Logs from your program will appear here!")
-
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -22,9 +20,21 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	_, err = conn.Write([]byte(logic.HttpResponseString("1.1", "200", "OK", "", "")))
-	if err != nil {
-		fmt.Println("Error writing to connection: ", err.Error())
-		os.Exit(1)
+
+	request := logic.GetHTTPRequest(conn)
+	path := logic.GetHTTPRequestPath(request)
+
+	if path == "/" {
+		_, err = conn.Write([]byte(logic.CreateHTTPResponseString("1.1", "200", "OK", "", "")))
+		if err != nil {
+			fmt.Println("Error writing to connection: ", err.Error())
+			os.Exit(1)
+		}
+	} else {
+		_, err = conn.Write([]byte(logic.CreateHTTPResponseString("1.1", "404", "Not Found", "", "")))
+		if err != nil {
+			fmt.Println("Error writing to connection: ", err.Error())
+			os.Exit(1)
+		}
 	}
 }
