0c6b32a2d57e0b99dce589a0f7c9e21da3c66011
zbyju <zbynek.jurica@gmail.com>
2024-05-14 17:52:59 +0200

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -9,18 +9,8 @@ import (
 	"github.com/codecrafters-io/http-server-starter-go/app/logic"
 )
 
-func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
-
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
 
 	request := logic.GetHTTPRequest(conn)
 	path := logic.GetHTTPRequestPath(request)
@@ -28,7 +18,7 @@ func main() {
 	headers := logic.ParseHTTPHeaders(headersStr)
 
 	if path == "/" {
-		_, err = conn.Write([]byte(logic.CreateHTTPResponseString("1.1", "200", "OK", "", "")))
+		_, err := conn.Write([]byte(logic.CreateHTTPResponseString("1.1", "200", "OK", "", "")))
 		if err != nil {
 			fmt.Println("Error writing to connection: ", err.Error())
 			os.Exit(1)
@@ -36,23 +26,40 @@ func main() {
 	} else if strings.HasPrefix(path, "/echo/") {
 		str := strings.TrimPrefix(path, "/echo/")
 		res := logic.CreateHTTPResponse(200, map[string]string{"Content-Type": "text/plain"}, str)
-		_, err = conn.Write([]byte(res))
+		_, err := conn.Write([]byte(res))
 		if err != nil {
 			fmt.Println("Error writing to connection: ", err.Error())
 			os.Exit(1)
 		}
 	} else if strings.HasPrefix(path, "/user-agent") {
 		res := logic.CreateHTTPResponse(200, map[string]string{"Content-Type": "text/plain"}, headers["User-Agent"])
-		_, err = conn.Write([]byte(res))
+		_, err := conn.Write([]byte(res))
 		if err != nil {
 			fmt.Println("Error writing to connection: ", err.Error())
 			os.Exit(1)
 		}
 	} else {
-		_, err = conn.Write([]byte(logic.CreateHTTPResponseString("1.1", "404", "Not Found", "", "")))
+		_, err := conn.Write([]byte(logic.CreateHTTPResponseString("1.1", "404", "Not Found", "", "")))
 		if err != nil {
 			fmt.Println("Error writing to connection: ", err.Error())
 			os.Exit(1)
 		}
+	}
+}
+
+func main() {
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
+	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
 	}
 }
